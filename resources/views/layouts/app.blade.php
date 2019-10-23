5da417adf35409bd5e1cb8453570b4555e1b12ef
Erik Jacobsen <erikjacobsen@Eriks-MacBook-Pro.local>
2019-10-22 21:22:48 -0400

    adding in bootstrap styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
